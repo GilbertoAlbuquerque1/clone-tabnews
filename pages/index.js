14f973ccbfcb1d52f0767fac1b0cfabdc91d95fb
Gilberto Albuquerque <gilbertogian.rs@gmail.com>
2025-08-19 11:43:49 +0000

    muda o nome

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home () {
-    return <h1>Graziela eu te amo, se estiver lendo e isso hoje tem dá um sorrizinho, se não for ter nada dá um mortal pra trais</h1>
+    return <h1>Graziela eu te amo, se estiver lendo e isso hoje vamos NAMORAR dá um sorrizinho, se não for ter nada dá um mortal pra trais</h1>
 }
 export default Home;
