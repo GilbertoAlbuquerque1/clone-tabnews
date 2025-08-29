2eb943bac92e86ae44c692335ad91e426866d0f7
Gilberto Albuquerque <gilbertogian.rs@gmail.com>
2025-08-29 17:07:45 +0000

    ´Mensagem para day´

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 function Home() {
   return (
     <h1>
-      Te amo More, seu reizinho aqui <3,Já que está lendo isso hoje Paulo vai entrar em ação, se não quizer vai ter que dar um mortal pra trais. 
-    </h1>)
+      Te amo More, seu reizinho aqui, Já que está lendo isso hoje Paulo vai
+      entrar em ação, se não quizer vai ter que dar um mortal pra trais.
+    </h1>
+  );
 }
