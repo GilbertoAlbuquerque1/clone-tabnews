cdd742c8d89c4122cdcb99ced91cd89b31e0a665
Gilberto Albuquerque <gilbertogian.rs@gmail.com>
2025-08-29 16:57:07 +0000

    Mudar nome para mensagem de Day

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 function Home() {
   return (
     <h1>
-      Te amo More, seu reizinho aqui <3, 
-      Já que está lendo isso hoje Paulo vai entrar em ação, se não quizer vai ter que dar um mortal pra trais. 
+      Te amo More, seu reizinho aqui <3, Já que está lendo isso hoje Paulo vai entrar em ação, se não quizer vai ter que dar um mortal pra trais. 
     </h1>
   );
 }
