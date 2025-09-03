223b93ada11052322c2cce0468f56a429c0ed427
Gilberto Albuquerque <gilbertogian.rs@gmail.com>
2025-09-03 19:45:14 +0000

    add and  files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,9 @@
-function status(request, response) {
-  response
-    .status(200)
-    .json({ chave: "Cristo vive, Ele é a solução para seus problemas." });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result.rows);
+  response.status(200).json({ chave: "Cristo vive, Ele é a solução para seus problemas." });
 }
 
 export default status;
